92f699832e26f0e3bc65e01a40656c73d5f5d602
DollyG <dollsgoitse@yahoo.com>
2018-06-25 12:18:21 +0000

    added heroku app to allowed hosts

--- ecommerce/settings/base.py
+++ ecommerce/settings/base.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['e-commerce-class-dollyg.c9users.io']
+ALLOWED_HOSTS = ['e-commerce-class-dollyg.c9users.io', 'ecommercedolly.herokuapp.com']
 
 
 # Application definition
